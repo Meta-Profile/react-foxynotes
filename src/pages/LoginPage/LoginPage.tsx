942997712a7cba97fea01e0ecd1835d89ea7b318
Яков Панов <diegoling33@yandex.ru>
2022-02-28 19:18:10 +0300

    добавлены eslint и prettir

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,5 @@
-import {FC} from "react";
+import { FC } from 'react';
 
 export const LoginPage: FC = () => {
-  return <div>1</div>
+    return <div>1</div>;
 };
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,17 +1,17 @@
 import {
     BrowserRouter,
     Routes,
-    Route,
-} from "react-router-dom";
-import {FC} from "react";
-import LoginPage from "../pages/LoginPage";
+    Route
+} from 'react-router-dom';
+import { FC } from 'react';
+import LoginPage from '../pages/LoginPage';
 
 export const Router: FC = (props) => {
-    const {children} = props;
+    const { children } = props;
     return <BrowserRouter>
         {children}
         <Routes>
-            <Route path="/" element={<LoginPage />} />
+            <Route path='/' element={<LoginPage />} />
         </Routes>
-    </BrowserRouter>
-}
+    </BrowserRouter>;
+};
